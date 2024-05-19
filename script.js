138a630c537a1662bf6deca44fdb24e2a33ff581
florenciagithub <135426046+florenciagithub@users.noreply.github.com>
2024-05-19 16:23:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function convertName() {
     let inputName = document.getElementById('inputName').value;
     let outputName = inputName
-        .replace(/ä/g, 'ä')
-        .replace(/ö/g, 'ö')
-        .replace(/ü/g, 'ü')
-        .replace(/Ä/g, 'Ä')
-        .replace(/Ö/g, 'Ö')
-        .replace(/Ü/g, 'Ü')
-        .replace(/ß/g, 'ß')
+        .replace(/ä/g, 'a')
+        .replace(/ö/g, 'o')
+        .replace(/ü/g, 'u')
+        .replace(/Ä/g, 'A')
+        .replace(/Ö/g, 'O')
+        .replace(/Ü/g, 'U')
+        .replace(/ß/g, 'ss')
         .replace(/[^a-zA-Z0-9äöüÄÖÜß_]/g, '-')  // Ersetzt alle Sonderzeichen außer ä, ö, ü, Ä, Ö, Ü, ß, _ durch -
         .replace(/-+/g, '-')  // Ersetzt mehrere aufeinanderfolgende - durch ein einzelnes -
         .trim();
