6061ef0869f6e0c3536db945ab6bab2ec860ea3c
florenciagithub <135426046+florenciagithub@users.noreply.github.com>
2024-05-19 15:18:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function convertName() {
     let inputName = document.getElementById('inputName').value;
+    // Erlaubt ä, ö, ü und ß neben alphanumerischen Zeichen
     let outputName = inputName.replace(/[^a-zA-Z0-9äöüß]/g, ' ').replace(/\s+/g, ' ').trim();
     document.getElementById('outputName').textContent = outputName;
 }
 
 function convertNameAndCapitalize() {
     let inputName = document.getElementById('inputName').value;
+    // Erlaubt ä, ö, ü und ß neben alphanumerischen Zeichen
     let outputName = inputName.replace(/[^a-zA-Z0-9äöüß]/g, ' ').replace(/\s+/g, ' ').trim();
     outputName = outputName.charAt(0).toUpperCase() + outputName.slice(1).toLowerCase();
     document.getElementById('outputName').textContent = outputName;
